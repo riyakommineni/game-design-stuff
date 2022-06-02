b929eb67c3e02791ae80b62f08a85df9a95fd82b
riyakommineni <komminenir24@greenhill.org>
2022-06-02 11:22:30 -0500

    added problems 2,3,4

--- stringassignment.py
+++ stringassignment.py
@@ -14,3 +14,21 @@ print(str1[4])
 #case 1
 str2='JohnDipPeta'
 print(str2[4:7])
+
+#case 2
+str3='JaSonAy'
+print(str3[2:5])
+
+#Problem 2
+str4='Ault'
+str5='Kelly'
+print(str4[0:2]+str5+str4[2:4])
+
+#Problem 3
+str6='America'
+str7='Japan'
+print(str6[0]+str7[0]+str6[3]+str7[2]+str6[6]+str7[4])
+
+#Problem 4
+str8='PyNaTive'
+print(str8[1]+str8[3]+str8[5:8]+str8[0]+str8[2]+str8[4])
