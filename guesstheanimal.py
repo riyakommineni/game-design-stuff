ff632d9abf73d06e490d1e4ad505a267253bb591
riyakommineni <komminenir24@greenhill.org>
2022-06-06 09:25:27 -0500

    updated

--- guesstheanimal.py
+++ guesstheanimal.py
@@ -16,7 +16,7 @@ import random   #this is needed for the random loop
 random.choice(list1)  #random loop is now for list1
 
 
-guess= input('enter an animal name:') #for player to guess an animal from list
+guess=input('enter an animal name: ') #for player to guess an animal from list
 
 if guess in random.choice(list1) : 
     print('Congratulations you guessed the animal!')
@@ -27,3 +27,4 @@ else:
 #should i print the whole list for the player to know what is available to guess?
 #i couldn't figure out the while loop. it would've been useful
 #need to figure out how to give option to continue guessing if they lost or to start a new round if they guess correctly (while loop probably)
+#this needs to somehow upload to github ugh!
